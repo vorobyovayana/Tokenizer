49dac70d19b77febd35352326d504799a682c626
vorobyovayana <39862834+vorobyovayana@users.noreply.github.com>
2018-11-09 13:15:46 +0300

    Update test_ToIndex.py

--- test_ToIndex.py
+++ test_ToIndex.py
@@ -1,4 +1,5 @@
 import unittest
+import shelve
 from indexation import ToIndex
 
 class ToIndex:
@@ -8,6 +9,7 @@ class ToIndex:
         Create an object of the class ToTokenize.
         """
         self.token = ToIndex()
+        text_file = open( "test_text.txt", "w")
 
 
     def test_filename_exists(self):
@@ -40,18 +42,22 @@ class ToIndex:
         
         db = dict(self.token.index("test_text"))
         files = os.listdir
-        extensions=[".dat", ".dir", ".bak"]
-        file_presence= False
+        extensions = [".dat", ".dir", ".bak"]
+        database_presence = False
         for file in files:
             for extension in extensions:
                 if file.endswith(extension):
-                    file_presence = True
-        self.assertEqual(file_presence, True)
+                    database_presence = True
+                    os.remove("D:\python_projects\\" + file + extension)
+        self.assertTrue(file_presence)
+        text_file.tearDown()
         
 
     def test_if_all_tokens_are_unique(self):
-
+        
         db = dict(self.token.index("test_text"))
+        text_file.open( "test_text", 'w')
+        text_file.write( "mama mila ramu")
         ref_dict = {'mama':{'test_text':[0,4]}, 'mila': {'test_text':[5,9]}, 'ramu': {'test_text': [10, 14]}}
         self.assertEqual(len(db), 3)
         self.assertEqual(ref['mama'], db['mama'])
@@ -61,10 +67,13 @@ class ToIndex:
             for extension in extensions:
                 if file.endswith(extension):
                     os.remove("D:\python_projects\\" + file + extension)
+        text_file.tearDown()
 
     def test_if_not_all_tokens_are_unique(self):
 
         db = dict(self.token.index("test_text_not_unique"))
+        text_file.open( "test_text", 'w')
+        text_file.write( "mama mama mila ramu")
         ref_dict= { 'mama': {'test_text_not_unique': [0, 4, 5, 9], 'mila': {'test_text_not_unique': [10, 14]}}, 'ramu': {'test_text_not_unique': [15, 19]}}
         self.assertEqual(len(db), 4)
         self.assertEqual(ref['mama'], db['mama'])
@@ -74,7 +83,7 @@ class ToIndex:
             for extension in extensions:
                 if file.endswith(extension):
                     os.remove("D:\python_projects\\" + file + extension)
-    
+        text_file.tearDown()
         
 
 if __name__ == '__main__':
