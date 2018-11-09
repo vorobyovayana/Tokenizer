b94e8a4bfc789f57b6c4468d005500300d5ea257
vorobyovayana <39862834+vorobyovayana@users.noreply.github.com>
2018-11-09 13:41:42 +0300

    Update test_ToIndex.py

--- test_ToIndex.py
+++ test_ToIndex.py
@@ -3,41 +3,41 @@ import shelve
 import os
 from indexation import ToIndex
 
-class ToIndex:
+class ToIndex(unittest.testcase):
 
     def setUp(self):
         """
         Create an object of the class ToTokenize.
         """
-        self.token = ToIndex()
+        self.indexes = ToIndex()
         self.text_file = open( "test_text.txt", "x")
 
 
-    def test_filename_exists(self):
+    def test_file_does_not_exists(self):
         
         with self.assertRaises(ValueError):
-            self.token.index("D:\nofile")
+            self.indexes.index("D:\nofile")
          
 
     def test_input_is_a_number(self):
         
         with self.assertRaises(TypeError):
-            self.token.index(42)
+            self.indexes.index(42)
     
     def test_input_is_a_list(self):
         
         with self.assertRaises(TypeError):
-            self.token.index([1, 2, 3, 4])
+            self.indexes.index([1, 2, 3, 4])
  
     def test_input_is_a_dict(self):
         
         with self.assertRaises(TypeError):
-            self.token.index({1:2, 3:4})
+            self.indexes.index({1:2, 3:4})
 
     def test_input_is_a_tuple(self):
         
         with self.assertRaises(TypeError):
-            self.token.index((1, 2, 3, 4))
+            self.idexes.index((1, 2, 3, 4))
 
     def test_db_was_created(self):
         
@@ -51,12 +51,12 @@ class ToIndex:
                     database_presence = True
                     os.remove("D:\python_projects\\" + file + extension)
         self.assertTrue(file_presence)
-        text_file.tearDown()
+        os.remove("D:\python_projects\\" + "text_file.txt")
         
 
     def test_if_all_tokens_are_unique(self):
         
-        db = dict(self.token.index("test_text"))
+        db = dict(self.indexes.index("test_text"))
         text_file.open( "test_text", 'w')
         text_file.write( "mama mila ramu")
         ref_dict = {'mama':{'test_text':[0,4]}, 'mila': {'test_text':[5,9]},
@@ -70,11 +70,11 @@ class ToIndex:
             for extension in extensions:
                 if file.endswith(extension):
                     os.remove("D:\python_projects\\" + file + extension)
-        text_file.tearDown()
+        os.remove("D:\python_projects\\" + "text_file.txt")
 
     def test_if_not_all_tokens_are_unique(self):
 
-        db = dict(self.token.index("test_text_not_unique"))
+        db = dict(self.indexes.index("test_text"))
         text_file.open( "test_text", 'w')
         text_file.write( "mama mama mila ramu")
         ref_dict= { 'mama': {'test_text_not_unique': [0, 4, 5, 9]},
@@ -89,7 +89,7 @@ class ToIndex:
             for extension in extensions:
                 if file.endswith(extension):
                     os.remove("D:\python_projects\\" + file + extension)
-        text_file.tearDown()
+        os.remove("D:\python_projects\\" + "text_file.txt")
         
 
 if __name__ == '__main__':
