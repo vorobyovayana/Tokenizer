075d3d4d706b719b94b2d836c9232e681beeedcd
vorobyovayana <39862834+vorobyovayana@users.noreply.github.com>
2018-11-16 13:57:25 +0300

    Update test_ToIndex.py

--- test_ToIndex.py
+++ test_ToIndex.py
@@ -6,35 +6,65 @@ from indexation import ToIndex
 class ToIndex(unittest.TestCase):
 
     def setUp(self):
-        
-        self.indexes = ToIndex('database')
+        """
+        This method creates an indexer, a position and a text file.
+        """
+        self.indexer = ToIndex('database')
+        self.position = Position()
         self.text_file = open("test_text.txt", "w")
+  
+    def tearDown(self):
+        """
+        This method destroys database and file text.
+        """
+        files = os.listdir
+        extensions = [".dat", ".dir", ".bak"]
+        for single_file in files:
+            if single_file == "database": 
+                    os.remove(single_file)
+            else:        
+                for extension in extensions:
+                    if single_file.startswith("database" + extension):                                                            
+                        os.remove(file + extension)
+        os.remove(text_file.txt)
         
     
     def test_file_does_not_exists(self):
-        
+        """
+        If the file does not exist raise ValueError.
+        """        
         with self.assertRaises(ValueError):
-            self.indexes.index("D:\nofile")
+            self.indexer.index("D:\nofile")
          
 
     def test_input_is_a_number(self):
-        
+        """
+        If the input is a number raise TypeError.
+        """        
         with self.assertRaises(TypeError):
-            self.indexes.index(42)
+            self.indexer.index(42)
     
     def test_input_is_a_list(self):
-        
+        """
+        If the input is a list raise TypeError.
+        """ 
         with self.assertRaises(TypeError):
-            self.indexes.index([1, 2, 3, 4])
+            self.indexer.index([1, 2, 3, 4])
  
-    def test_input_is_a_dict(self):
-        
+    def test_input_is_a_tuple(self):
+        """
+        If the input is a tuple raise TypeError.
+        """ 
         with self.assertRaises(TypeError):
-            self.idexes.index((1, 2, 3, 4))
+            self.idexer.index((1, 2, 3, 4))
 
     def test_db_was_created(self):
-        
-        db = dict(self.indexes.index("test_text"))
+        """
+        Test that the database was created.
+        """
+        self.text_file.write( "mama mila ramu")
+        self.text_file.close("test_text")
+        db = dict(self.indexer.index("test_text"))
         files = os.listdir
         extensions = [".dat", ".dir", ".bak"]
         database_presence = False
@@ -42,58 +72,42 @@ class ToIndex(unittest.TestCase):
             for extension in extensions:
                 if single_file.startswith("database" + extension):
                     database_presence = True
-                    os.remove(single_file + extension)
-                elif single_file = "database":
+                elif single_file == "database":
                     database_presence = True
-                    os.remove(single_file)
         self.assertTrue(file_presence)
-        os.remove(text_file.txt)
         
 
     def test_if_all_tokens_are_unique(self):
-
+        """
+        Test that indexer runs correctly if each token occurs only once.
+        """
         self.text_file.write( "mama mila ramu")
         self.text_file.close("test_text")
-        db = dict(self.indexes.index("test_text"))               
-        ref_dict = {'mama':{'test_text':[0,4]}, 'mila': {'test_text':[5,9]},
-                    'ramu': {'test_text': [10, 14]}
+        db = dict(self.indexer.index("test_text"))               
+        ref_dict = {
+        'mama' : {'test_text':[self.position.start,self.position.end]},
+        'mila' : {'test_text':[self.position.start,self.position.end]},
+        'ramu' : {'test_text': [self.position.start, self.position.end14]}
         }
         self.assertEqual(len(db), 3)
         self.assertEqual(ref['mama'], db['mama'])
-        files = os.listdir
-        extensions =[".dat", ".dir", ".bak"]
-        for single_file in files:
-            for extension in extensions:
-                if single_file.startswith("database" + extension):
-                    os.remove(file + extension)
-                elif single_file = "database":
-                    database_presence = True
-                    os.remove(single_file)
-        os.remove(text_file.txt)
 
     def test_if_not_all_tokens_are_unique(self):
-
+        """
+        Test that indexer runs correctly if some tokens occur more than once.
+        """
         self.text_file.write( "mama mama mila ramu")
         self.text_file.close("test_text")
-        db = dict(self.indexes.index("test_text"))       
-        ref_dict = { 'mama': {'test_text': [0, 4, 5, 9]},
-                    'mila': {'test_text': [10, 14]},
-                    'ramu': {'test_text': [15, 19]}
+        db = dict(self.indexer.index("test_text"))       
+        ref_dict = {
+        'mama': {'test_text': [self.position.start, self.position.end, self.position.start, self.position.end]},
+        'mila': {'test_text': [self.position.start, self.position.end]},
+        'ramu': {'test_text': [self.position.start, self.position.end]}
         }
         self.assertEqual(len(db), 4)
         self.assertEqual(ref['mama'], db['mama'])
-        files = os.listdir
-        extensions =[".dat", ".dir", ".bak"]
-        for single_file in files:
-            for extension in extensions:
-                if single_file.startswith("database" + extension):
-                    os.remove(file + extension)
-                elif single_file = "database":
-                    database_presence = True
-                    os.remove(single_file)
-        os.remove(text_file.txt)
-        
+   
+
 
 if __name__ == '__main__':
     unittest.main()
-        
