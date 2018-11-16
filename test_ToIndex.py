95eadd4022f989f91b5d8098781de5f7d205d48b
vorobyovayana <39862834+vorobyovayana@users.noreply.github.com>
2018-11-16 12:58:27 +0300

    Update test_ToIndex.py

--- test_ToIndex.py
+++ test_ToIndex.py
@@ -3,14 +3,14 @@ import shelve
 import os
 from indexation import ToIndex
 
-class ToIndex(unittest.testcase):
+class ToIndex(unittest.TestCase):
 
     def setUp(self):
         
-        self.indexes = ToIndex()
-        self.text_file = open( "test_text.txt", "x")
-
-
+        self.indexes = ToIndex('database')
+        self.text_file = open("test_text.txt", "w")
+        
+    
     def test_file_does_not_exists(self):
         
         with self.assertRaises(ValueError):
@@ -39,55 +39,64 @@ class ToIndex(unittest.testcase):
 
     def test_db_was_created(self):
         
-        db = dict(self.token.index("test_text"))
+        db = dict(self.indexes.index("test_text"))
         files = os.listdir
         extensions = [".dat", ".dir", ".bak"]
         database_presence = False
-        for file in files:
+        for single_file in files:
             for extension in extensions:
-                if file.endswith(extension):
+                if single_file.startswith("database" + extension):
                     database_presence = True
-                    os.remove("D:\python_projects\\" + file + extension)
+                    os.remove(single_file + extension)
+                elif single_file = "database":
+                    database_presence = True
+                    os.remove(single_file)
         self.assertTrue(file_presence)
-        os.remove("D:\python_projects\\" + "text_file.txt")
+        os.remove(text_file.txt)
         
 
     def test_if_all_tokens_are_unique(self):
         
-        db = dict(self.indexes.index("test_text"))
-        text_file.open( "test_text", 'w')
-        text_file.write( "mama mila ramu")
+        db = dict(self.indexes.index("test_text"))        
+        self.text_file.write( "mama mila ramu")
+        self.text_file.close("test_text")
         ref_dict = {'mama':{'test_text':[0,4]}, 'mila': {'test_text':[5,9]},
                     'ramu': {'test_text': [10, 14]}
         }
         self.assertEqual(len(db), 3)
         self.assertEqual(ref['mama'], db['mama'])
         files = os.listdir
-        extensions=[".dat", ".dir", ".bak"]
-        for file in files:
+        extensions =[".dat", ".dir", ".bak"]
+        for single_file in files:
             for extension in extensions:
-                if file.endswith(extension):
-                    os.remove("D:\python_projects\\" + file + extension)
-        os.remove("D:\python_projects\\" + "text_file.txt")
+                if single_file.startswith("database" + extension):
+                    os.remove(file + extension)
+                elif single_file = "database":
+                    database_presence = True
+                    os.remove(single_file)
+        os.remove(text_file.txt)
 
     def test_if_not_all_tokens_are_unique(self):
 
-        db = dict(self.indexes.index("test_text"))
-        text_file.open( "test_text", 'w')
-        text_file.write( "mama mama mila ramu")
-        ref_dict= { 'mama': {'test_text_not_unique': [0, 4, 5, 9]},
-                    'mila': {'test_text_not_unique': [10, 14]},
-                    'ramu': {'test_text_not_unique': [15, 19]}
+        db = dict(self.indexes.index("test_text"))       
+        self.text_file.write( "mama mama mila ramu")
+        self.text_file.close("test_text")
+        ref_dict = { 'mama': {'test_text': [0, 4, 5, 9]},
+                    'mila': {'test_text': [10, 14]},
+                    'ramu': {'test_text': [15, 19]}
         }
         self.assertEqual(len(db), 4)
         self.assertEqual(ref['mama'], db['mama'])
         files = os.listdir
-        extensions=[".dat", ".dir", ".bak"]
-        for file in files:
+        extensions =[".dat", ".dir", ".bak"]
+        for single_file in files:
             for extension in extensions:
-                if file.endswith(extension):
-                    os.remove("D:\python_projects\\" + file + extension)
-        os.remove("D:\python_projects\\" + "text_file.txt")
+                if single_file.startswith("database" + extension):
+                    os.remove(file + extension)
+                elif single_file = "database":
+                    database_presence = True
+                    os.remove(single_file)
+        os.remove(text_file.txt)
         
 
 if __name__ == '__main__':
