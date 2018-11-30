dc58666326ecb7e26b99fbcfa6a8d475187b3cd2
vorobyovayana <39862834+vorobyovayana@users.noreply.github.com>
2018-11-30 14:10:00 +0300

    Update indexation.py

--- indexation.py
+++ indexation.py
@@ -1,11 +1,11 @@
 from tokenization import ToTokenize
-from tokenization import TokenWithType
 import shelve
 
 class Position(self, start, length):
     def __init__(self):
         self.start = start
         self.end = start + length
+        self.tokenizer = ToTokenize()
 
     def __repr__(self):
         
@@ -22,9 +22,9 @@ class ToIndex:
         # Open file
         text_file = open(file_name, 'r')
         # Read file and save as a string
-        text_string = text_file.read
+        text_string = text_file.read()
         # Tokenize the string and write it in the variable 'tokens'.
-        tokens = tokenize_reduced(text_string)
+        tokens = self.tokenizer.tokenize_reduced(text_string)
         # Open database with name 'database'
         db = shelve.open('database')
 
@@ -32,16 +32,15 @@ class ToIndex:
             # Create an object of class Position for a current token
             position = Position(token.start, len(token.wordform))
             # If the token is not in the database, add it.
-            dict(db).setdefault(token, {})
-            # If the filename is not in the dictionary, add it
-            db[token].setdefault(file_name)
+            db.setdefault(token, {})
             # Write the dictionary 'db[token]' to a variable 'file_pos_dict'
             file_pos_dict = db[token]
+            # If the filename is not in the dictionary, add it
+            file_pos_dict.setdefault(file_name, [])
             # Add to the end of the list of positions position of the current token
             file_pos_dict[file_name].append(position)
         db.close()
-                
-                    
+                   
 if __name__ == '__main__':
     a = ToIndex()
 a.index("text.txt")
