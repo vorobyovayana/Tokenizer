324d288098a08912260780a804d94a32ae825dd0
vorobyovayana <39862834+vorobyovayana@users.noreply.github.com>
2018-12-19 10:32:23 +0300

    Update indexation.py

--- indexation.py
+++ indexation.py
@@ -33,6 +33,31 @@ class Position:
         This method is needed to compare the objects of class Position
         """
         return self.start == obj.start and self.end == obj.end
+
+class PositionByString:
+    
+     def __init__(self, start, end, string):
+        """
+        The method is needed to create positions of tokens --
+        objects that have two attributes:
+        @param: the start of the token
+        @param: the end of the token
+        """
+        self.start = start
+        self.end = end
+        self.string = string
+
+     def __repr__(self):
+        """
+        This method creates a string representation of a position.
+        """
+        return str(self.start) + ', ' + str(self.end) + ',' + str(self.string)
+
+     def __eq__(self, obj):
+        """
+        This method is needed to compare the objects of class Position.
+        """
+        return (self.start == obj.start and self.end == obj.end) and self.string == obj.string
     
 
 class ToIndex:
@@ -84,12 +109,42 @@ class ToIndex:
             # Use method '.setdefault()' to write the token, its file_name
             # and position to the database.
             self.db.setdefault(token.wordform, {}).setdefault(file_name, []).append(position)
-
+            
         # Use '.sync()' to save the database.
         self.db.sync()
-                   
+
+    def index_by_string(self, file_name):
+        # Raise TypeError if the input type is not string      
+        if not isinstance(file_name, str):
+            raise(TypeError)
+        # Raise ValuError if filename doesn't exist.
+        files = os.listdir()
+        if file_name not in files:
+            raise(ValueError)
+        # Create an object of ToTokenize.
+        tokenizer = ToTokenize()
+        # Open file
+        text_file = open(file_name, 'r')
+        
+        for num, string in enumerate(text_file):
+           tokens = tokenizer.tokenize_reduced(string)
+        text_file.close()   
+        for token in tokens:
+            # For each token in the list create an object of Position.
+            position = PositionByString(token.start, token.start + len(token.wordform), num)
+            # Use method '.setdefault()' to write the token, its file_name
+            # and position to the database.
+            self.db.setdefault(token.wordform, {}).setdefault(file_name, []).append(position)
+        print(self.db['mama'])    
+        # Use '.sync()' to save the database.
+        self.db.sync()
+
+
+
+                
+                    
 if __name__ == '__main__':
     a = ToIndex('database')
-    a.index("text.txt")
-    a.index("text2.txt")
-
+    a.index_by_string("text.txt")
+    #a.index("text.txt")
+    #a.index("text2.txt")
