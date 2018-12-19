8b954713ae5e3f9e88d01fcfa73220b4095698f7
vorobyovayana <39862834+vorobyovayana@users.noreply.github.com>
2018-12-19 10:32:00 +0300

    Update test_ToIndex.py

--- test_ToIndex.py
+++ test_ToIndex.py
@@ -3,6 +3,7 @@ import shelve
 import os
 from indexation import ToIndex
 from indexation import Position
+from indexation import PositionByString
 
 class TestToIndex(unittest.TestCase):
 
@@ -86,12 +87,12 @@ class TestToIndex(unittest.TestCase):
         Test that indexer runs correctly if each token occurs only once.
         """
         text_file = open("test_text.txt", "w")
-        text_file.write("mama mila ramu")
+        text_file.write("mama мыла ramu")
         text_file.close()
         self.indexer.index('test_text.txt')
         db = shelve.open('database')               
         ref_dict = {'mama' : {'test_text.txt':[Position(0,4)]},
-                    'mila' : {'test_text.txt':[Position(5,9)]},
+                    'мыла' : {'test_text.txt':[Position(5,9)]},
                     'ramu' : {'test_text.txt': [Position(10,14)]}
                     }
         
@@ -143,7 +144,89 @@ class TestToIndex(unittest.TestCase):
         self.assertEqual(ref_dict, dict(db))
         os.remove("test_text.txt")
         os.remove("another_test_text.txt")
+        
+class TestToIndexByString(unittest.TestCase):
+
+    def setUp(self):
+        """
+        In this method we create an indexer.
+        """
+        self.indexer = ToIndex('database')
+        self.maxDiff = None
+       
+  
+    def tearDown(self):
+        """
+        In this method we delete the indexer and destroy the database.
+        """
+        del self.indexer
+        files = os.listdir()
+
+        for single_file in files:
+            if single_file == "database": 
+                os.remove(single_file)
+            else:        
+
+                if single_file.startswith('database.'):                                                            
+                    os.remove(single_file)
+
+    def test_file_does_not_exists(self):
+        """
+        If the file does not exist raise ValueError.
+        """        
+        with self.assertRaises(ValueError):
+            self.indexer.index_by_string("nofile.txt")
+         
+
+    def test_input_is_a_number(self):
+        """
+        If the input is a number raise TypeError.
+        """        
+        with self.assertRaises(TypeError):
+            self.indexer.index_by_string(42)
+
+    def test_db_was_created(self):
+        """
+        Test that the database was created.
+        """
+        
+        db = dict(shelve.open('database'))
+        text_file = open("test_text.txt", "w")
+        text_file.write("mama mila ramu")
+        text_file.close()
+        self.indexer.index_by_string('test_text.txt')
+        files = os.listdir()
+        database_presence = False
+        for single_file in files:
+            if single_file == "database":
+                database_presence = True
+            else:
+                if single_file.startswith("database."):
+                    database_presence = True               
+        self.assertTrue(database_presence)
+        os.remove("test_text.txt")
+
+    def test_the_program_runs_ok(self):
+        text_file = open('test_text.txt', 'w')
+        text_file.write('mama мыла ramu')
+        text_file.close()
+        self.indexer.index_by_string('test_text.txt')
+        another_text_file = open("another_test_text.txt", 'w')
+        another_text_file.write('mama')
+        another_text_file.close()
+        self.indexer.index_by_string('another_test_text.txt')
+        db = shelve.open('database')
+        ref_dict = {
+                    'mama': {'another_test_text.txt': [PositionByString(0,4,0)],
+                             'test_text.txt': [PositionByString(0,4,0)]},
+                    'мыла': {'test_text.txt': [PositionByString(5,9,0)]},
+                    'ramu': {'test_text.txt': [PositionByString(10,14,0)]}
+                    }
+        
+        self.assertEqual(ref_dict, dict(db))
+        os.remove("test_text.txt")
+        os.remove("another_test_text.txt")
+        
 
 if __name__ == '__main__':
     unittest.main()
-
