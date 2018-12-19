2143a841c6b2a223f2c2bc5276e6d3d3537d9597
vorobyovayana <39862834+vorobyovayana@users.noreply.github.com>
2018-12-19 09:54:11 +0300

    Update indexation.py

--- indexation.py
+++ indexation.py
@@ -1,61 +1,95 @@
+"""
+This module was made to index tokens.
+It consists of two classes: class Position and class ToIndex.
+"""
 from tokenization import ToTokenize
 from tokenization import TokenWithType
 import shelve
 import os
 
 class Position:
+    """
+    The class is needed to define the start and the end of a token being indexed.
+    """
+    
     def __init__(self, start, end):
+        """
+        The method is needed to create positions of tokens --
+        objects that have two attributes:
+        @param: the start of the token
+        @param: the end of the token
+        """
         self.start = start
         self.end = end
         
     def __repr__(self):
-        
+        """
+        This method creates a string representation of a position.
+        """
         return str(self.start) + ', ' + str(self.end)
 
     def __eq__(self, obj):
-
+        """
+        This method is needed to compare the objects of class Position
+        """
         return self.start == obj.start and self.end == obj.end
     
 
 class ToIndex:
-    
+    """
+    The class is needed to index a file. 
+    """
     def __init__(self, db_name):
-
+        """
+        In this method we create a database where indexed tokens
+        are going to be stored.
+        """
         self.db = shelve.open(db_name, writeback=True)
 
     def __del__(self):
-
+        """
+        In this method we close the database.
+        """
         self.db.close()
                
     def index(self, file_name):
-        #self.db.clear()
+        """
+        This method gets indexes for the tokens in a file.
+        @param: the name of the file
+        """
+
         # Raise TypeError if the input type is not string      
         if not isinstance(file_name, str):
             raise(TypeError)
-
+        # Raise ValuError if filename doesn't exist.
         files = os.listdir()
         if file_name not in files:
             raise(ValueError)
-        
+        # Create an object of ToTokenize.
         tokenizer = ToTokenize()
         # Open file
         text_file = open(file_name, 'r')
         # Read file and save as a string
         text_string = text_file.read()
+        # Close the file.
         text_file.close()
+        # Tokenize the string and write resulting tokens (only alphabetical
+        # and digital ones) to the list 'tokens'
         tokens = tokenizer.tokenize_reduced(text_string)
+        
+        
         for token in tokens:
+            # For each token in the list create an object of Position.
             position = Position(token.start, token.start + len(token.wordform))
+            # Use method '.setdefault()' to write the token, its file_name
+            # and position to the database.
             self.db.setdefault(token.wordform, {}).setdefault(file_name, []).append(position)
 
-        
+        # Use '.sync()' to save the database.
         self.db.sync()
-        
-        
-
-                
-                    
+                   
 if __name__ == '__main__':
     a = ToIndex('database')
     a.index("text.txt")
     a.index("text2.txt")
+
