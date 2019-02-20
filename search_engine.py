ed7edb9f2cad05c34ba5cec171215dc17d2913bb
vorobyovayana <39862834+vorobyovayana@users.noreply.github.com>
2019-02-20 12:11:51 +0300

    Update search_engine.py

--- search_engine.py
+++ search_engine.py
@@ -1,34 +1,19 @@
-'''
-The module allows to search through documents given one or multiple queries.
-'''
 import shelve
 import os
 from indexation import PositionByLine
 from tokenization import ToTokenize
 from tokenization import TokenWithType
-
 class SearchEngine:
-    '''
-    The class produces search. It consists of two methods: search() and multi_search().
-    '''
 
     def __init__(self, db_name):
-        '''
-        Open a database.
-        '''
+        
         self.db = shelve.open(db_name)
         
     def __del__(self):
-        '''
-        Close the database.
-        '''
 
         self.db.close()
         
     def search(self, query):
-        '''
-        This method produced search through documents given one-word query.
-        '''
         # Raise TypeError if the input type is not string      
         if not isinstance(query, str):
             raise TypeError
@@ -38,7 +23,7 @@ class SearchEngine:
             raise ValueError('Empty query')
         
         # Raise ValueError if the query doesn't match any key in the database.
-        if query not in self.db.keys():
+        if query not in self.db:
             return ValueError("The query doesn't match any key in the database")
         
         # Return dictionary with files that match the query as keys and
@@ -46,9 +31,7 @@ class SearchEngine:
         return self.db[query]
 
     def multi_search(self, query):
-        '''
-        This method produced search through documents given multi-word query.
-        '''
+        
         # Raise TypeError if the input type is not string 
         if not isinstance(query, str):
             raise TypeError
@@ -64,62 +47,44 @@ class SearchEngine:
         # the variable 'query'.
         query = tokenizer.tokenize_reduced(query)
         
-        # This list will contain lists of documents for each word of the query.
-        list_of_docs = []
+        # This list will contain documents in which the current word can
+        # be found.
+        docs = []
         
-        # This list will contain dictionaries with files as keys and positions as values.
-        dictionaries = []
         for word in query:
-        
             # Raise ValueError if the query doesn't match any key in the database.
-            if word.wordform not in self.db.keys():
+            if word.wordform not in self.db:
                 raise ValueError("The query doesn't match any key in the database")
-            else:
-                # This list will contain documents in which the current word can
-                # be found.
-                docs = []
-                # Append documents in which the current word can be found.
-                for key in self.db[word.wordform].keys():
-                    docs.append(key)
-                # Append the dictionary with files as keys and positions as values.
-                dictionaries.append(self.db[word.wordform])
-                
-                # Apend the list of document for the current word
-                # to the list of documents for all words of the query.
-                list_of_docs.append(docs)
-                
-        # Turn the 'list_of_docs' into a set and write it
-        # to the variable 'list_of_docs'.
-        list_of_docs = set(list_of_docs)
+            # Append documents in which the current word can be found.
+            docs.append(self.db[word.wordform])
+        # Turn the first document name into a set and write it into 'files'
+        files = set(docs[0])
         
-        # Write the first list of documents to the variable 'document'.
-        document = list_of_docs[0]
-        
-        for doc in list_of_docs[1:]:
-            # Intersect the current list of documents with the following one
-            # and write it to the variable 'document'.
-            
-            document &= list_of_docs[1:]
+        for document in docs[1:]:
+            # Each document name from the list "docs" (except for the first one)
+            # we intersect with 'files'
+            files &= set(document)
             
         # This dictionary will be returned.
         result = {}
         
-        # 'document' contains files in which every word from
-        # the query is present.
-        for doc_name in document:
-            # For each dictionary with files as keys and positions as values: 
-            for dictionary in dictionaries:
-                
+        for file_name in files:
+            for doc_name in docs:
                 # Write the names of the document and the corresponding positions into 'result'.
-                result.setdefault(doc_name, []).extend(dictionary[doc_name])
+                result.setdefault(file_name, []).extend(doc_name[file_name])
         return result
+    
+    def get_context_window(self, query):
+        tokenizer = ToTokenize()
+        query = tokenizer.tokenize_reduced(query)
+        for word in query:
+            line_num = word.line
+        
+            
 
             
     
     
 if __name__ == '__main__':
     a = SearchEngine('database')
-    print(a.search('Анна'))
-    print(a.multi_search('Анна'))
-          
-
+    print(a.multi_search('Анна Павловна'))
