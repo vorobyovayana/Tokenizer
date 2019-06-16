edc5f61741fecab4c085169d3000ba731762dd0e
vorobyovayana <39862834+vorobyovayana@users.noreply.github.com>
2019-06-17 02:26:20 +0300

    Update test_ContextWindows.py

--- test_ContextWindows.py
+++ test_ContextWindows.py
@@ -18,7 +18,6 @@ class TestGetOneContextWindow(unittest.TestCase):
         text.write('ooh la la мама мыла раму123  frf34')
         text.close()
         indexer.index_by_line('test_text.txt')
-
         del indexer
         
     def tearDown(self):
@@ -27,7 +26,7 @@ class TestGetOneContextWindow(unittest.TestCase):
         os.remove('test_text.txt')
 
 
-    def test_get_one_cw_correctly(self):
+    def test_get_one_cw_correctly(self): #runs ok
 
         positions = PositionByLine(10, 14, 0)
         window_size = 1
@@ -36,21 +35,36 @@ class TestGetOneContextWindow(unittest.TestCase):
         self.assertEqual(ref_cw, actual_cw)
 
 
-    def test_window_size_is_bigger_than_line(self):
+    def test_window_size_is_bigger_than_line(self): #runs ok
 
         positions = PositionByLine(10, 14, 0)
         window_size = 5
         ref_cw = ContextWindow([PositionByLine(10, 14, 0)],0,34, 'ooh la la мама мыла раму123  frf34')
         actual_cw = self.get_cw.get_one_cw(window_size, 'test_text.txt', positions)
         self.assertEqual(ref_cw, actual_cw)
+        
 
-    def test_windows_are_united_correctly(self):
+    def test_windows_are_united_correctly(self): # a problem
 
         window_size = 1
-        print(self.search.multi_search('мама мыла'))
-        actual_cw = self.get_cw.unite_cws(self.search.multi_search('мама мыла'), window_size)
-        ref_cw = [([(0, 4, 0), (5, 9, 0)], 0, 14, 'мама мыла раму долго')]
+        actual_cw = self.get_cw.get_united_cws(self.search.multi_search('мама мыла'), window_size)
+        ref_cw = [([(10, 14, 0), (15, 19, 0)], 7, 23, 'ooh la la мама мыла раму123  frf34')]
         self.assertEqual(ref_right_cont, actual_right_cont)
+
+    def test_windows_are_extended_correctly(self): # a problem
+
+        window_size = 1
+        actual_cw = self.get_cw.get_extended_cws(self.search.multi_search('мама мыла'), window_size)
+        ref_cw = [([(10, 14, 0), (15, 19, 0)], 0, 33, 'ooh la la мама мыла раму123  frf34')]
+
+    def test_windows_are_bolded_correctly(self):
+        
+        # is not finished
+        
+
+        window_size = 1
+        actual_cw = self.get_cw.get_bold_cws(self.search.multi_search('мама мыла'), window_size)
+        ref_cw = [([(10, 14, 0), (15, 19, 0)], 7, 23, 'ooh la la мама мыла раму123  frf34')]
 
 
         
