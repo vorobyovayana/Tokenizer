fede5a3a4c802b153322e84888af58d9bce16267
vorobyovayana <39862834+vorobyovayana@users.noreply.github.com>
2019-11-13 23:12:31 +0300

    Update web_server.py

--- web_server.py
+++ web_server.py
@@ -7,17 +7,17 @@ from indexation import PositionByLine
 
 class RequestHandler(BaseHTTPRequestHandler):
     '''
-    This module creates a html page on which a user
+    This class creates a html page on which a user
     can perform search by clicking a button 'search.
+    It also shows the results of a search for the given query.
     '''
 
     def do_GET(self):
         '''
-        This module creates a html page on which a user
-        can perform search by clicking a button 'search.
+        This method creates a html page on which a user
+        can perform search by clicking a button 'search'.
         Handling of a request of type GET.
-        '''
-        
+        '''        
         # Send response to the browser that the requested page was found.
         self.send_response(200)        
         # Establish that the content must be interpreted as
@@ -40,7 +40,9 @@ class RequestHandler(BaseHTTPRequestHandler):
         self.wfile.write(bytes(html, encoding = "utf-8"))
            
     def do_POST(self):
-
+        '''
+        This method shows the search results.
+        '''
         # Create an object of the FieldStorage to read
         # the form contents from standard input or the environment 
         form = cgi.FieldStorage(fp = self.rfile, headers = self.headers, environ ={'REQUEST_METHOD':'POST'})
@@ -53,24 +55,20 @@ class RequestHandler(BaseHTTPRequestHandler):
         # Create an object of the 'Contexter' class to get context windows.
         self.contexter = Contexter()
         # Get context windows and make the query words bold.
-        cws = self.contexter.get_bold_cws(search_results, 3)
-        
-        
-        # Send tags to indicate the beginning of the html body of the page.
-        self.wfile.write(bytes("""<html><form><body>""", encoding = "cp1251"))
-        # Start an ordered list.
-        self.wfile.write(bytes('<ol>', encoding = "utf-8"))
+        cws = self.contexter.get_bold_cws(search_results, 3)       
+        # Send tags to indicate the beginning of the html body of the page
+        # and start an ordered list.
+        self.wfile.write(bytes('<html><form><body><ol>', encoding = "cp1251"))
         # Post each file name as an element of an ordered list.
         for i, file_name in enumerate(cws):
-            self.wfile.write(bytes('<li><p>%s</p>' % file_name, encoding = "utf-8"))            
+            self.wfile.write(bytes('<li><p>%s</p></i>' % file_name, encoding = "utf-8"))            
             self.wfile.write(bytes('<ul>', encoding = "cp1251"))
             # Post each quote from the file as an element of an unordered list.
             for cw in cws[file_name]:
-                self.wfile.write(bytes('<li><p>%s</p>' % cw.line, encoding = "cp1251"))
+                self.wfile.write(bytes('<li><p>%s</p></i>' % cw.line, encoding = "cp1251"))
             self.wfile.write(bytes('</ul>', encoding = "cp1251"))
         # Send tags to mark the end of the html body.
-        self.wfile.write(bytes("""</ol></form></body></html>""", encoding = "cp1251"))
-            
+        self.wfile.write(bytes('</ol></form></body></html>', encoding = "cp1251"))          
           
 if __name__ == '__main__':       
     server = HTTPServer(('', 80), RequestHandler)
